0d1e4c77f5fefc02004b697f1270c2f13dad7938
Adarsh Bhoutekar <adarshbhoutekar@gmail.com>
2026-04-18 16:50:53 +0530

    first commit

---
+++ src/ui/base_layout.py
@@ -0,0 +1,90 @@
+import streamlit as st
+
+def style_background_home():
+    st.markdown(
+        """
+        <style>
+            .stApp{
+                background: #5856F2 !important; /* home */
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+def style_background_dashboard():
+    st.markdown(
+        """
+        <style>
+            .stApp{
+                background: #red !important; /* dashboard */
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+def style_base_layout():
+    st.markdown(
+        """
+        <style>
+        @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap');
+        
+        
+            /*Hide top bar of streamlit */
+            #mainMenu , footer, header{
+                visibility: hidden;
+            }
+            
+            .block-container{
+                padding-top: 1.5rem !important;
+            }
+            
+            h1{
+                font-family: 'Climate Crisis', sans-serif !important;
+                font-size: 3.5rem !important;
+                line-height: 0.9 !important;
+                margin-bottom: 0rem !important;
+            }
+            
+            h3, h4, p{
+                font-family: 'Noto Serif', serif !important;
+                # font-optical-sizing: auto !important;
+                # font-weight: 400 !important;
+                # font-style: normal !important;
+            }
+        
+            .stButton > button{
+                border-radius: 1.5rem !important;
+                background: #5865F2 !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+            }
+
+            .stButton > button[kind="secondary"] {
+                border-radius: 1.5rem !important;
+                background: #EB459E !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+            }
+
+            .stButton > button[kind="tertiary"]{
+                border-radius: 1.5rem !important;
+                background: black !important;
+                color: white !important;
+                padding: 10px 20px !important;
+                border: none !important;
+                transition: transform 0.25s ease-in-out !important;
+            }
+            
+            .stButton > button:hover{
+                transform:scale(1.05)
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
